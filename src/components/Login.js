c1f902b7d736f4fa5422840557b52d9c430b2a4c
jenkobud <jbudnechky@fi.uba.ar>
2022-04-18 16:27:19 -0300

    Created new folder /components & Login.js example component

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+const logName = "loggi el login"
+const Login = () => {
+    return(
+        //Devuelve JSX
+        <h1>Login devolviendo un JSX llamado <i>{logName}</i></h1>
+    );
+}
+
+export default Login;
