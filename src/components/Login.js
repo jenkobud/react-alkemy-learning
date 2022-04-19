323d2944c7bb45455d10062857b8941d5140172e
jenkobud <jenkobudne@gmail.com>
2022-04-19 18:39:31 -0300

    Added needed npm packages, implemented MaterialUI components on Login (password kind of bad)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useState } from 'react'
 import axios from 'axios';
 import swal from 'sweetalert';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const submitHandler = e => {
   e.preventDefault();
@@ -33,23 +41,79 @@ const validateEmailFormat = (email) => {
   return (regexEmail.test(email));
 }
 
-const Label = (props) => {
+/* const Label = (props) => {
   return(
     <label>
       <span>{props.name}:</span>
       <input type={props.type} name={props.name} id={props.id}></input>
     </label>
   );
-}
+} */
 
 const Login = () => {
+  //State
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    showPassword: false
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   //Devuelve JSX
   return(
       <form onSubmit={submitHandler}>
         <div className = "form-child"><h2>Login</h2></div>
         <div className = "form-child">
-          <Label name="email" type="email" id="emailId"/>
-          <Label name="password" type="password" id="passwordId"/>
+          <TextField
+            required
+            id="emailId"
+            label="Email"
+            defaultValue=""
+            name="email"
+          />
+          {/*<TextField
+            required
+            id="passwordId"
+            label="Password"
+            defaultValue=""
+            name="password"
+            type="password"
+          />*/}
+          {/* source: Mui->TextField */}
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="passwordId"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+          {/*End Mui->TextField*/ }
         </div>
         <button type="submit"> Login </button>
       </form>
