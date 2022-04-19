53741585e95b05c617519f52cb6148b1817e5c09
jenkobud <jbudnechky@fi.uba.ar>
2022-04-19 12:08:14 -0300

    Created basic Login interface with bague error-handling.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,53 @@
-const logName = "loggi el login"
+import axios from 'axios';
+
+const submitHandler = e => {
+  e.preventDefault();
+  console.log("submitHandler log:");
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  if(email === "" || password === "") alert("Email or Password is empty.");
+  else if(!validateEmailFormat(email)) alert("Invalid Email format.");
+  console.log(email);
+  console.log(password);
+  
+  /* Debería estar una logica de validación de datos de usuario */
+
+  axios.post('url_de_API_a_conectar',{email, password})
+  .then(res => { 
+    console.log(res);
+    console.log(res.data);
+  });
+
+}
+
+const validateEmailFormat = (email) => {
+  const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return (regexEmail.test(email));
+}
+
+const Label = (props) => {
+  return(
+    <label>
+      <span>{props.name}:</span>
+      <input type={props.type} name={props.name} id={props.id}></input>
+    </label>
+  );
+}
+
 const Login = () => {
-    return(
-        //Devuelve JSX
-        <h1>Login devolviendo un JSX llamado <i>{logName}</i></h1>
-    );
+  //Devuelve JSX
+  return(
+      <>
+    <form onSubmit={submitHandler}>
+      <div className = "form-child"><h2>Login</h2></div>
+      <div className = "form-child">
+        <Label name="email" type="email" id="emailId"/>
+        <Label name="password" type="password" id="passwordId"/>
+      </div>
+      <button type="submit"> Login </button>
+    </form>
+    </>
+  );
 }
 
 export default Login;
