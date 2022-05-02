d75b86dcbdff51a6f812b31caf673177e038ff19
jenkobud <jbudnechky@fi.uba.ar>
2022-05-02 17:40:16 -0300

    Minimal imporvements on SearchBar component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+// Styles
 import '../styles/SearchBar.css';
 import '../styles/Layout.css';
+
 const SearchBar = ({placeholder='Buscar...'}) => {
   const [keyword, setKeyword] = useState('');
+  const handleOnChange = e => { setKeyword(e.target.value); }
+  useEffect(() => {
+    //Buscar las peliculas/series que concuerden con keyword...
+  }, [keyword]);
   return(
     <div className='searchbar-container'>
-      <input type='text' placeholder={placeholder} value={keyword}/>
+      <input type='text' placeholder={placeholder} onChange={handleOnChange}/>
     </div>
   );
 }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,18 +1,21 @@
 import * as React from 'react';
+
+// Own Components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import SearchBar from '../components/SearchBar';
 
 const SearchPage = () => {
     return(
         <div className="home-page">
             <Header />
-            <SearchPage placeholder='Título, personaje o género..'/>
+            <SearchBar placeholder='Título, personaje o género'/>
             <div className='general-container'>
-                search
+            search content
             </div>
             <Footer />
         </div>
-    )
+    );
 }
 
 export default SearchPage;
