84dca3ccc11ab6f2081ebb7b87545589c50d3a63
jenkobud <jenkobudne@gmail.com>
2022-05-06 17:59:23 -0300

    Fixed genres using states, added tabs on moviePage, style improvements.

--- src/components/TabsPanel.js
+++ src/components/TabsPanel.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -17,9 +18,7 @@ const TabPanel = (props) => {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
+        <Box sx={{ p: 3 }}> {children} </Box>
       )}
     </div>
   );
@@ -31,42 +30,28 @@ TabPanel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-const a11yProps = (index) => {
-  return {
-    id: `tab-${index}`
-  };
-}
-
-export default function TabsPanel({tabsName=[], }) {
+export default function TabsPanel({tabs=[] }) {
   /* props: {
-      tabsName = [v1,v2,v3,..],
-      tabContent = Debería pasarle el contenido?.. o trabajarlo x afuera?..
+      [{tabName, tabContent}, {tabName, tabContent}, {tabName, tabContent},...]
     }
   */
-  const [value, setValue] = React.useState(0);
-
+  const [value, setValue] = useState(0);
   const handleChange = (event, newValue) => { setValue(newValue); };
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} >
-          {tabsName.map((tab) => {
-            return(<Tab label={tab} id={tab} /> );
+          {tabs.map((tab, index) => {
+            return(<Tab label={tab.name} id={`tab-${index}`} key={tab.name}/> );
           })}
-          {/* <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} /> */}
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
+      {tabs.map((tab,index)=>{
+        return(
+          <TabPanel value={value} index={index} key={index}> {tab.content} </TabPanel>
+        );
+      })}
     </Box>
   );
 }
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,7 +1,7 @@
 // Hooks & Components
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useParams, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
@@ -26,19 +26,63 @@ import '../styles/Layout.css';
 // Services
 import { getMovieById, getImageUrl, getFHDImageUrl } from '../servicies/movieDB.service';
 
+const intToHoursAndMinutes = (minutes) => {
+  //returns a String.
+  let h = Math.round(minutes / 60);
+  return (`${h} h ${minutes - (h * 60)} min`);
+}
+
 const MoviePage = () => {
 
+  const navigate = useNavigate();
   const token = localStorage.getItem('token');
   const [movieInfo,setMovieInfo] = useState({});
   const[loader, setLoader] = useState(true);
+  const [movieGenres, setMovieGenres] = useState([]);
   const { id = 0 } = useParams();
+  
   useEffect(() => {
     //Después cambiarlo con fowardRef..
     getMovieById(id)
-      .then((res) => { setMovieInfo(res.data); })
-      .catch(err => { console.log('Error de themovieDB -> '+ err); })
-      .finally( setTimeout( ()=>{setLoader(false)} ,800) );
-  },[]);
+      .then((res) => { 
+        setMovieInfo(res.data); 
+        setMovieGenres(res.data.genres);
+      })
+      .catch(err => { 
+        console.log('Error de themovieDB -> ' + err);
+        navigate(-1); })
+      .finally(setTimeout(() => { setLoader(false) }, 800));
+  }, []);
+
+  const detailsContent =
+    <div className='detail-tab-content'>
+      <div className='fifty-percent flex-col-start'>
+        <h4>{movieInfo.original_title}</h4>
+        <p>{movieInfo.overview}</p>
+      </div>
+      <div className='flex-col-start'>
+        <h5>Duración</h5>
+        {intToHoursAndMinutes(movieInfo.runtime)}
+        <h5>Fecha de estreno</h5>
+        {movieInfo.release_date}
+        <h5>Genero</h5>
+        {movieGenres.map(genre=>{ return (<span key={genre.id}>{genre.name} </span>) }) }
+        <h5>Clasificación</h5>
+        <span>{movieInfo.adult ? '+18' : 'ATP'}</span>
+      </div>
+      <div className='flex-col-start'>
+        <h5>Dirigido por</h5>
+        <span>Director name</span>
+        <h5>Elenco</h5>
+        Ipso lorum
+      </div>
+    </div>;
+
+  const tabsContent = [{ name: 'SUGERENCIAS', content: 'SUGERENCIAS CONTENT' },
+    { name: 'EXTRAS', content: 'EXTRAS CONTENT' },
+    { name: 'VERSIONES', content: 'VERSIONES CONTENT' },
+    { name: 'DETALLES', content: detailsContent }];
+
 
   return (
     <>
@@ -58,7 +102,7 @@ const MoviePage = () => {
                   <div className='poster-container'>
                     <img alt={movieInfo.original_title} src={getImageUrl(movieInfo.poster_path)} />
                   </div>
-                  <div className='info-details border'>
+                  <div className='info-details'>
                     <h4>{movieInfo.original_title}</h4>
                     <a href={movieInfo.homepage} target="_blank">{movieInfo.homepage}</a>
                     <div className='tags-container'>
@@ -74,9 +118,10 @@ const MoviePage = () => {
                       <span>2021</span>
                       <span>148min</span>
                     </div>
-                    {/* <span>{genres.map((genre) => {
+                    <span>{movieGenres.toString()}</span>
+                    <span>{movieGenres.map((genre) => {
                     return(genre.name + ',');
-                  })}</span> */}
+                  })}</span>
 
                     <div className='tags-container'>
                       <Button className='button-watch' size="small" startIcon={<PlayArrowIcon />}>
@@ -89,9 +134,8 @@ const MoviePage = () => {
                     <p>{movieInfo.overview}</p>
                   </div>
                   <div className='tabs'>
-                    <TabsPanel />
+                    <TabsPanel tabs={tabsContent}/>
                   </div>
-
                 </div>
               </article>
             </div>
